349fae9145cc3edfb33906b717d9c6f5dfd821b2
Aliaksei Valovik <aliaksei.valovik@alreadymedia.com>
2025-10-08 20:37:16 +0300

    New release

--- sgw-plugin.php
+++ sgw-plugin.php
@@ -11,7 +11,7 @@ if (!defined("ABSPATH")) die;
 /**
  * Plugin Name:       SGW Plugin
  * Description:       Sportsgateway Plugin
- * Version:           1.1.5
+ * Version:           1.1.6
  * Author:            AM
  * Text Domain:       sgw-plugin
  * Domain Path:       /languages
